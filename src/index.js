d6c02d830ce022af80abd8952886516814f1e82d
cesarsiguencia <cesar.siguencia@gmail.com>
2022-05-12 21:02:39 -0400

    changning repo name

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// for React version 18,
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <App />
+</React.StrictMode>,
+document.getElementById("root")
+)
+
+ 
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
