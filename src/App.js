29dda592300c7567cd490006f31cec4b93135c87
cesarsiguencia <cesar.siguencia@gmail.com>
2022-05-12 22:04:28 -0400

    added About and Nav components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react'; // needs to be imported in every component file
+// import logo from './logo.svg';
 import './App.css';
+import About from './components/About'
+import Nav from './components/Nav'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( // this is JSX, HTML represented in JS, this works due to webpack and React, react basically uses JSX as document.createElement()
+  <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
+  </div>
   );
 }
 
